3550d2948bbd7f54057e35d428ce9d3c68825dc8
Your Name <sumitkaroo6699@gmail.com>
2024-06-07 21:20:32 +0530

    Added Testcases using get method, conditional method and browser method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.sql.SQLOutput;
 import java.util.List;
 import java.util.Set;
 
