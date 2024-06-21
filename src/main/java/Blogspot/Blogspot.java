025b51139ad070e536d5a2aaa30dbecf2ece481e
Your Name <sumitkaroo6699@gmail.com>
2024-06-21 20:12:00 +0530

    Test

--- src/main/java/Blogspot/Blogspot.java
+++ src/main/java/Blogspot/Blogspot.java
@@ -21,6 +21,7 @@ public class Blogspot {
 
         WebDriver driver=new ChromeDriver();
 
+
         driver.get("https://testautomationpractice.blogspot.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
